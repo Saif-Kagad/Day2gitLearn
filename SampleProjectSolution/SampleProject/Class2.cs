adeb39c52158432c60c2bcdf8c9faefb96247ea1
Saif-Kagad <76032601+Saif-Kagad@users.noreply.github.com>
2020-12-16 10:48:26 +0530

    Create Class2.cs

---
+++ SampleProjectSolution/SampleProject/Class2.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProject
+{
+    class Class2
+    {
+        public void Second()
+        {
+            Console.WriteLine("hello from github");
+        }
+           
+    }
+}
